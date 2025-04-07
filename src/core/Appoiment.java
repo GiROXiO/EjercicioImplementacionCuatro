d7afd8101a03e2ed0988709855d5e06fca129d4f
Jeronimo <jecastroam@gmail.com>
2025-04-06 23:13:13 -0500

    Final Commit: se muestran los resultados en consola

--- src/core/Appoiment.java
+++ src/core/Appoiment.java
@@ -1,19 +1,19 @@
 package core;
 
 public class Appoiment {
-    private Doctor doctor;
+    private JuniorDoctor doctor;
     private Patient patient;
 
-    public Appoiment(Doctor doctor, Patient patient) {
+    public Appoiment(JuniorDoctor doctor, Patient patient) {
         this.doctor = doctor;
         this.patient = patient;
     }
 
-    public Doctor getDoctor() {
+    public JuniorDoctor getDoctor() {
         return doctor;
     }
 
-    public void setDoctor(Doctor doctor) {
+    public void setDoctor(JuniorDoctor doctor) {
         this.doctor = doctor;
     }
 
--- src/core/Hospital.java
+++ src/core/Hospital.java
@@ -3,6 +3,7 @@ package core;
 import java.util.ArrayList;
 
 public class Hospital {
+
     private ArrayList<Team> teams;
     private ArrayList<Ward> wards;
 
@@ -10,85 +11,116 @@ public class Hospital {
         this.teams = new ArrayList<>();
         this.wards = new ArrayList<>();
     }
-    
-    public void addTeam(int teamId, int consultantDoctorId){
+
+    public void addTeam(int teamId, int consultantDoctorId) {
         ConsultantDoctor cd = new ConsultantDoctor(consultantDoctorId);
         Team team = new Team(teamId, cd);
         cd.setLeaderOf(team);
         this.teams.add(team);
     }
-    
-    public Team getTeam(int teamId){
-        for(Team team : teams){
-            if(team.getId()==teamId)
+
+    public Team getTeam(int teamId) {
+        for (Team team : teams) {
+            if (team.getId() == teamId) {
                 return team;
+            }
         }
         return null;
     }
-    
-    public void addJuniorDoctor(Team team, int jdId){
+
+    public void addJuniorDoctor(Team team, int jdId) {
         JuniorDoctor jd = new JuniorDoctor(jdId);
         jd.setTeam(team);
         team.addJuniorDoctor(jd);
     }
-    
-    public void addWard(int wardId){
+
+    public void addWard(int wardId) {
         this.wards.add(new Ward(wardId));
     }
-    
-    public void addPatient(Ward ward, Team team, int patientId){
+
+    public void addPatient(Ward ward, Team team, int patientId) {
         ward.addPatient(new Patient(patientId, team, ward));
         team.addPatient(new Patient(patientId, team, ward));
     }
-    
-    public Ward getWard(int wardId){
-        for(Ward ward : this.wards){
-            if(ward.getId()==wardId)
+
+    public Ward getWard(int wardId) {
+        for (Ward ward : this.wards) {
+            if (ward.getId() == wardId) {
                 return ward;
+            }
         }
         return null;
     }
-    
-    public void assignPatientDoctor(Patient patient, int doctorId){
-        patient.addDoctor(this.getDoctor(patient.getTeam().getId()+doctorId));
+
+    public void assignPatientDoctor(Patient patient, int doctorId) {
+        patient.addDoctor(this.getDoctor(patient.getTeam().getId() + doctorId));
     }
-    
-    public void assignAppoiment(Patient patient, int doctorId){
-        JuniorDoctor doctor = this.getDoctor(patient.getTeam().getId()+doctorId);
+
+    public void assignAppoiment(Patient patient, int doctorId) {
+        JuniorDoctor doctor = this.getDoctor(patient.getTeam().getId() + doctorId);
         Appoiment apmt = new Appoiment(doctor, patient);
         patient.addAppoiment(apmt);
         doctor.addAppoiment(apmt);
     }
-    
-    public Patient getPatient(int patientId){
-        for(Ward ward : this.wards){
-            for(Patient patient : ward.getPatients()){
-                if(patient.getId()==patientId)
+
+    public Patient getPatient(int patientId) {
+        for (Ward ward : this.wards) {
+            for (Patient patient : ward.getPatients()) {
+                if (patient.getId() == patientId) {
                     return patient;
+                }
             }
         }
         return null;
     }
-    
-    public JuniorDoctor getDoctor(int doctorId){
-        for(Team team : this.teams){
-            for(JuniorDoctor doctor : team.getDoctors()){
-                if(doctor.getId()==doctorId)
+
+    public JuniorDoctor getDoctor(int doctorId) {
+        for (Team team : this.teams) {
+            for (JuniorDoctor doctor : team.getDoctors()) {
+                if (doctor.getId() == doctorId) {
                     return doctor;
+                }
             }
         }
         return null;
     }
-    
-    public void numberDoctorsPatient(){
-        
+
+    public void numberDoctorsPatient() {
+        for (Ward ward : this.wards) {
+            for (Patient patient : ward.getPatients()) {
+                int c = 0;
+                for (Doctor doctor : patient.getDoctors()) {
+                    c++;
+                }
+                System.out.println("Patient " + patient.getId() + " has " + c + " doctors");
+            }
+        }
     }
-    
-    public void numberPatientsTeam(){
-        
+
+    public void numberPatientsTeam() {
+        for (Team team : this.teams) {
+            int c = 0;
+            for (Patient patient : team.getPatients()) {
+                c++;
+            }
+            System.out.println("Team " + team.getId() + " has " + c + " patients");
+        }
     }
-    
-    public void relationAppoiments(){
-        
+
+    public void relationAppoiments() {
+        for (Ward ward : this.wards) {
+            for (Patient patient : ward.getPatients()) {
+                int c = 0;
+                ArrayList<JuniorDoctor> doctores = new ArrayList<>();
+                for (Appoiment appoiment : patient.getAppoiments()) {
+                    c++;
+                    doctores.add(appoiment.getDoctor());
+                }
+                System.out.println("Patient " + patient.getId() + " has " + c + " appoiments");
+                for (JuniorDoctor doctor : doctores) {
+                    System.out.println("Patient " + patient.getId() + " has an appoiment with the doctor " + doctor.getId());
+                }
+            }
+        }
     }
 }
